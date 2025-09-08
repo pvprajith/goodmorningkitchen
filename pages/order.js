ad2d0d15f517364b4152ce296c372e2019c9619b
pvprajith <pvprajith@gmail.com>
2025-09-08 13:45:48 +0530

    order.js

--- pages/order.js
+++ pages/order.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { loadCart, total, clearCart, removeFromCart, BRAND } from "../lib/cart";
 
